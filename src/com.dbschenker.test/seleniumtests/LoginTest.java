1f039701c187e2dbbf03634869687be2a1f48d37
DESKTOP-SQ3LAVL\User <deb.jagadev@live.com>
2017-07-27 19:21:19 +1000

    ExcelUtility Added

--- src/com.dbschenker.test/seleniumtests/LoginTest.java
+++ src/com.dbschenker.test/seleniumtests/LoginTest.java
@@ -2,10 +2,14 @@ package seleniumtests;
 import com.dbschenker.framework.base.Browser;
 import com.dbschenker.framework.base.DriverContext;
 import com.dbschenker.framework.base.FrameworkInitialize;
+import com.dbschenker.framework.utilities.ExcelUtil;
+import jxl.read.biff.BiffException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import pages.LoginPage;
+
+import java.io.IOException;
 
 
 public class LoginTest extends FrameworkInitialize{
@@ -16,6 +20,14 @@ public class LoginTest extends FrameworkInitialize{
         initializeBrowser(Browser.browserType.IE);
 
         DriverContext.browser.goToUrl("http://auintratest01/mcwms_test/");
+
+        try {
+            ExcelUtil util = new ExcelUtil("\\resource\\TestData.xls","LoginDetails");
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @After
@@ -26,8 +38,10 @@ public class LoginTest extends FrameworkInitialize{
     @Test
     public void login() throws InterruptedException{
         CurrentPage =  GetInstance(LoginPage.class);
-        CurrentPage.As(LoginPage.class).login("debashishj","PHILIPSNSW","debj2017");
-
+        //CurrentPage.As(LoginPage.class).login("debashishj","PHILIPSNSW","debj2017");
+        CurrentPage.As(LoginPage.class).login(ExcelUtil.ReadCell("UserName",1),
+                ExcelUtil.ReadCell("Storer",1),
+                ExcelUtil.ReadCell("Password",1));
 
 
     }
--- src/com/dbschenker/framework/utilities/ExcelUtil.java
+++ src/com/dbschenker/framework/utilities/ExcelUtil.java
@@ -1,7 +1,67 @@
 package com.dbschenker.framework.utilities;
 
-/**
- *
- */
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
 public class ExcelUtil {
+
+    static Sheet wrksheet;
+    static Workbook wrkbook =null;
+    static Hashtable dict= new Hashtable();
+    //Create a Constructor
+    public ExcelUtil(String excelSheetPath, String sheetName) throws BiffException, IOException
+    {
+        //Initialize
+        wrkbook = Workbook.getWorkbook(new File(excelSheetPath));
+        //For Demo purpose the excel sheet path is hardcoded, but not recommended :)
+        wrksheet = wrkbook.getSheet(sheetName);
+        ColumnDictionary();
+    }
+
+    //Returns the Number of Rows
+    public static int RowCount()
+    {
+        return wrksheet.getRows();
+    }
+
+    //Returns the Cell value by taking row and Column values as argument
+    private static String ReadCell(int column,int row)
+    {
+        return wrksheet.getCell(column,row).getContents();
+    }
+
+    /*
+    method overloading
+         */
+    public static String ReadCell(String columnName, int rowNum){
+        return ReadCell(columnName,rowNum);
+    }
+
+    //Create Column Dictionary to hold all the Column Names
+    private static void ColumnDictionary()
+    {
+        //Iterate through all the columns in the Excel sheet and store the value in Hashtable
+        for(int col=0;col < wrksheet.getColumns();col++)
+        {
+            dict.put(ReadCell(col,0), col);
+        }
+    }
+
+    //Read Column Names
+    private static int GetCell(String colName)
+    {
+        try {
+            int value;
+            value = ((Integer) dict.get(colName)).intValue();
+            return value;
+        } catch (NullPointerException e) {
+            return (0);
+
+        }
+    }
 }
